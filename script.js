a5b3df2d664233e8e4ccbb64dc0054f5706b4ac0
Vusal313 <muradovvusal565@gmail.com>
2024-08-08 23:23:41 +0400

    New sounds

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
 const cells = document.querySelectorAll('.cell');
 const message = document.getElementById('message');
 const restartButton = document.getElementById('restart');
+const clickSound = document.getElementById('clickSound');
+const winSound = document.getElementById('winSound');
+const drawSound = document.getElementById('drawSound');
+const finalWinSound = document.getElementById('finalWinSound');
+const scoreX = document.getElementById('scoreX');
+const scoreO = document.getElementById('scoreO');
+const medal = document.getElementById('medal');
 
 let board = ['', '', '', '', '', '', '', '', ''];
 let currentPlayer = 'X';
 let gameActive = true;
+let score = { X: 0, O: 0 };
+let round = 0;
 
 const winningConditions = [
     [0, 1, 2],
@@ -20,6 +29,7 @@ const winningConditions = [
 function handleCellPlayed(clickedCell, clickedCellIndex) {
     board[clickedCellIndex] = currentPlayer;
     clickedCell.innerText = currentPlayer;
+    clickSound.play();
 }
 
 function handlePlayerChange() {
@@ -43,15 +53,21 @@ function handleResultValidation() {
     }
 
     if (roundWon) {
+        winSound.play();
         message.innerText = `Oyuncu ${currentPlayer} kazandı!`;
-        gameActive = false;
+        score[currentPlayer]++;
+        updateScore();
+        round++;
+        checkFinalWinner();
         return;
     }
 
     let roundDraw = !board.includes('');
     if (roundDraw) {
+        drawSound.play();
         message.innerText = 'Berabere!';
-        gameActive = false;
+        round++;
+        checkFinalWinner();
         return;
     }
 
@@ -76,6 +92,31 @@ function handleRestartGame() {
     gameActive = true;
     message.innerText = '';
     cells.forEach(cell => cell.innerText = '');
+}
+
+function updateScore() {
+    scoreX.innerText = score.X;
+    scoreO.innerText = score.O;
+}
+
+function checkFinalWinner() {
+    if (round === 3) {
+        gameActive = false;
+        if (score.X > score.O) {
+            message.innerText = 'Oyuncu X Şampiyon!';
+            finalWinSound.play();
+        } else if (score.O > score.X) {
+            message.innerText = 'Oyuncu O Şampiyon!';
+            finalWinSound.play();
+        } else {
+            message.innerText = 'Oyun Berabere!';
+        }
+        medal.style.display = 'block';
+    } else {
+        gameActive = true;
+        board = ['', '', '', '', '', '', '', '', ''];
+        cells.forEach(cell => cell.innerText = '');
+    }
 }
 
 cells.forEach(cell => cell.addEventListener('click', handleCellClick));
